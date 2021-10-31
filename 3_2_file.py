ac221713466fd559c93719caafda5c27fb8c49e8
133ASIRAK <92091145+133ASIRAK@users.noreply.github.com>
2021-10-31 21:48:12 +0900

    Add files via upload

---
+++ 3_2_file.py
@@ -0,0 +1,7 @@
+print(1+1)
+print(3*5)
+a=1; b=1; print(a+b)
+print("Hello World")
+a=1;
+b=1;
+a+b;
---
+++ 4_3_list.py
@@ -0,0 +1,16 @@
+students = ["egoing", "sori", "maru"]
+grades = [2, 1, 4]
+print(students[1])
+print("studnts[1]", students[1])
+print("len(students)", len(students))
+print("min(grades)", min(grades))
+print("min(grades)", max(grades))
+print("min(grades)", sum(grades))
+
+import statistics
+print("statistics.mean(grades)", statistics.mean(grades))
+
+import random
+print("random.choice(students)", random.choice(students))
+print("random.choice(grades)", random.choice(grades))
+
